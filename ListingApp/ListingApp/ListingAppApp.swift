a004df4e21e63228d0559f58867709bb13e1a983
zamandev <zamanm@calculated.com>
2024-09-10 07:52:31 -0400

    created the xcode project with swiftUI

---
+++ ListingApp/ListingApp/ListingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListingAppApp.swift
+//  ListingApp
+//
+//  Created by Dev on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
